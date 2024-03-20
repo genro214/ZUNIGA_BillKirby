83eb1f95143e72f173c05e32765424ddbbc1d2ea
genro214 <genro214@gmail.com>
2024-03-20 19:36:10 +0800

    ASSIGNMENT 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
+    path: 'assignment',
+    loadChildren: () => import('./assignment/assignment-module').then( m => m.AssignmentModule)
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
